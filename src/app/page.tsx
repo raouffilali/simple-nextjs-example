f122c6a32adcbf2f5fa7b1486f5abc662e47b583
Filali Abderaouf <filaliabderaouf54@gmail.com>
2024-02-13 06:10:56 +0100

    Refactor home page layout and add link to posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-<div className="h1">
-    <h1>Home</h1>
-    <Image
-      src="/vercel.svg"
-      alt="Vercel Logo"
-      width={72}
-      height={16}
-    />
-    <p>Welcome to the home page!</p>
-</div>
+    <div className="container mx-auto flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">HELLO 😀</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <p className="mb-12">Click below to see posts</p>
+      <Link href="/posts" style={{ borderRadius: '4px', color: 'black', backgroundColor: 'white', padding: '8px 16px', textDecoration: 'none' }}>
+        Click here
+      </Link>
+    </div>
   );
 }
